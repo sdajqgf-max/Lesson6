27f72ab6fc2c86ab14ba07b54033aa82dd807a75
sdajqgf-max <sdajqgf@gmail.com>
2026-02-02 09:57:15 -0800

    feat:averageSumOfSalary

--- task1.ts
+++ task1.ts
@@ -1,11 +1,28 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 // Написать функцию, которая будет высчитывать среднюю сумму чисел значений объекта
-
-const salaries: Record<string, number | string | null> = {
+type Salaries = Record<string, number | string | null>;
+const salaries = {
   alexander: 500,
   elena: 341.3,
   karina: "465",
   anastasia: 480.5,
   diana: 390,
   julia: null,
-};
+} as const;
+
+function averageSumOfSalaries(salaries: Salaries) {
+  let sumSalaries = 0;
+  let countSalary = 0;
+  for (const salaryKey in salaries) {
+    const salary = salaries[salaryKey];
+    if (salary != null) {
+      countSalary++;
+      sumSalaries += parseFloat(String(salary));
+    }
+  }
+  if (countSalary > 0) {
+    return sumSalaries / countSalary;
+  }
+  return 0;
+}
+console.log(averageSumOfSalaries(salaries));
